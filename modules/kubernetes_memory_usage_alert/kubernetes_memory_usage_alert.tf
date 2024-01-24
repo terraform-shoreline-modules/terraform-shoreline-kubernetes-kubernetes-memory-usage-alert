1caad59a484f2b66c218197c42de461bade2c542
shoreline-agent <63118884+shoreline-agent@users.noreply.github.com>
2024-01-24 09:04:02 -0800

    Add/update multiple files

--- modules/kubernetes_memory_usage_alert/kubernetes_memory_usage_alert.tf
+++ modules/kubernetes_memory_usage_alert/kubernetes_memory_usage_alert.tf
@@ -1,46 +1,46 @@
 resource "shoreline_notebook" "kubernetes_memory_usage_alert" {
   name       = "kubernetes_memory_usage_alert"
   data       = file("${path.module}/data/kubernetes_memory_usage_alert.json")
-  depends_on = [shoreline_action.invoke_cordon_drain_delete_node,shoreline_action.invoke_k8s_terminate_pod]
+  depends_on = [shoreline_action.invoke_cordon_and_drain_nodes,shoreline_action.invoke_terminate_pods_on_node]
 }
 
-resource "shoreline_file" "cordon_drain_delete_node" {
-  name             = "cordon_drain_delete_node"
-  input_file       = "${path.module}/data/cordon_drain_delete_node.sh"
-  md5              = filemd5("${path.module}/data/cordon_drain_delete_node.sh")
-  description      = "Reove node from cluster"
-  destination_path = "/agent/scripts/cordon_drain_delete_node.sh"
+resource "shoreline_file" "cordon_and_drain_nodes" {
+  name             = "cordon_and_drain_nodes"
+  input_file       = "${path.module}/data/cordon_and_drain_nodes.sh"
+  md5              = filemd5("${path.module}/data/cordon_and_drain_nodes.sh")
+  description      = "Remove node from cluster."
+  destination_path = "/tmp/cordon_and_drain_nodes.sh"
   resource_query   = "host"
   enabled          = true
 }
 
-resource "shoreline_file" "k8s_terminate_pod" {
-  name             = "k8s_terminate_pod"
-  input_file       = "${path.module}/data/k8s_terminate_pod.sh"
-  md5              = filemd5("${path.module}/data/k8s_terminate_pod.sh")
+resource "shoreline_file" "terminate_pods_on_node" {
+  name             = "terminate_pods_on_node"
+  input_file       = "${path.module}/data/terminate_pods_on_node.sh"
+  md5              = filemd5("${path.module}/data/terminate_pods_on_node.sh")
   description      = "Identify and terminate any resource-intensive pods on the impacted node(s) to free up memory."
-  destination_path = "/agent/scripts/k8s_terminate_pod.sh"
+  destination_path = "/tmp/terminate_pods_on_node.sh"
   resource_query   = "host"
   enabled          = true
 }
 
-resource "shoreline_action" "invoke_cordon_drain_delete_node" {
-  name        = "invoke_cordon_drain_delete_node"
-  description = "Reove node from cluster"
-  command     = "`chmod +x /agent/scripts/cordon_drain_delete_node.sh && /agent/scripts/cordon_drain_delete_node.sh`"
+resource "shoreline_action" "invoke_cordon_and_drain_nodes" {
+  name        = "invoke_cordon_and_drain_nodes"
+  description = "Remove node from cluster."
+  command     = "`chmod +x /tmp/cordon_and_drain_nodes.sh && /tmp/cordon_and_drain_nodes.sh`"
   params      = ["NODE_NAME"]
-  file_deps   = ["cordon_drain_delete_node"]
+  file_deps   = ["cordon_and_drain_nodes"]
   enabled     = true
-  depends_on  = [shoreline_file.cordon_drain_delete_node]
+  depends_on  = [shoreline_file.cordon_and_drain_nodes]
 }
 
-resource "shoreline_action" "invoke_k8s_terminate_pod" {
-  name        = "invoke_k8s_terminate_pod"
+resource "shoreline_action" "invoke_terminate_pods_on_node" {
+  name        = "invoke_terminate_pods_on_node"
   description = "Identify and terminate any resource-intensive pods on the impacted node(s) to free up memory."
-  command     = "`chmod +x /agent/scripts/k8s_terminate_pod.sh && /agent/scripts/k8s_terminate_pod.sh`"
+  command     = "`chmod +x /tmp/terminate_pods_on_node.sh && /tmp/terminate_pods_on_node.sh`"
   params      = ["NODE_NAME"]
-  file_deps   = ["k8s_terminate_pod"]
+  file_deps   = ["terminate_pods_on_node"]
   enabled     = true
-  depends_on  = [shoreline_file.k8s_terminate_pod]
+  depends_on  = [shoreline_file.terminate_pods_on_node]
 }
 
